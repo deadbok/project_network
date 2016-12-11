6c2d0864fca0e3e5ec46f2d4a4573571b90aa84b
Cristian Iacobanu <cristianiacobanu@gmail.com>
2016-12-11 17:55:40 +0100

    Added error message on ajax error

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,7 +14,14 @@ $(document).ready( function () {
 
 			// Show html
 			$("#md-file").html(converter.makeHtml(data));
+			// Show headings list (auto-generated)
 			$('#md-file').anchorific();
+		},
+		error: function() {
+			// Add .ajax-not-working class to body
+			$('body').addClass('ajax-not-working');
+			// Add error message to .ajax-error element
+			$('.ajax-error').fadeIn().html('<h4>Seems like your network settings are not able to connect to the internet.</h4>');
 		}
 	});
 
