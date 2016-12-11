77d3dfa7b5cf5b65da623614362b362f9b4dc98c
Cristian Iacobanu <cristianiacobanu@gmail.com>
2016-12-11 13:31:30 +0100

    Added site files toDMZ Server folder

---
+++ server-dmz-web/var/www/network.tools/README.md
@@ -0,0 +1,4 @@
+# Project Network
+## Group7 at EAL ITET2016
+
+Web page: [https://deadbok.github.io/project_network/](https://deadbok.github.io/project_network/)
---
+++ server-dmz-web/var/www/network.tools/assets/js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready( function () {
+
+	// Get element class as path variable
+	var path = $('#md-file').attr('class').split(' ').join('/');
+
+	// Load readme content
+	$.ajax({
+		url: 'https://raw.githubusercontent.com/deadbok/project_network/'+path+'/README.md',
+		dataType: 'text',
+		success: function(data) {
+
+			// Convert readme from markdown to html
+			var converter = new showdown.Converter({extensions: ['table', 'github']});
+
+			// Show html
+			$("#md-file").html(converter.makeHtml(data));
+			$('#md-file').anchorific();
+		}
+	});
+
+});
