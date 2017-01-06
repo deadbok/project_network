c07f3cf35008702704c22c61b9798845c2d301ba
Cristian Iacobanu <cristianiacobanu@gmail.com>
2017-01-06 20:58:06 +0100

    Small changes - merge resource files, add smooth scrolling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready( function () {
 
+	var target	= '.md-file';
+	var error_message	= '.ajax-error';
+
 	// Get element class as path variable
-	var path = $('#md-file').attr('class').split(' ').join('/');
+	var path = $(target).attr('id').split(' ').join('/');
 
 	// Load readme content
 	$.ajax({
@@ -13,16 +16,39 @@ $(document).ready( function () {
 			var converter = new showdown.Converter({extensions: ['table', 'github']});
 
 			// Show html
-			$("#md-file").html(converter.makeHtml(data));
+			$(target).html(converter.makeHtml(data));
 			// Show headings list (auto-generated)
-			$('#md-file').anchorific();
+			$(target).anchorific();
 		},
 		error: function() {
 			// Add .ajax-not-working class to body
 			$('body').addClass('ajax-not-working');
 			// Add error message to .ajax-error element
-			$('.ajax-error').fadeIn().html('<h4>Seems like your network settings are not able to connect to the internet.</h4>');
+			$(error_message).fadeIn().html('<h4>Seems like your network settings are not able to connect to the internet.</h4>');
 		}
+	});
+
+	// Get <header> height
+	var header = $('header').outerHeight(true);
+
+	// Prevent default jump-to action for anchor links
+	// Animate scroll if link statrs with '#' character
+	$('.anchorific').on('click', 'a[href^=\\#]', function(event){
+	    event.preventDefault();
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top - header
+	    }, 500);
+		return false;
+	});
+
+	// Prevent default jump-to action for anchor links
+	// Animate scroll if link includes name
+	$('.md-file').on('click', 'a[href^=\\#]', function(event){
+	    event.preventDefault();
+	    $('html, body').animate({
+	        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - header
+	    }, 500);
+		return false;
 	});
 
 });
