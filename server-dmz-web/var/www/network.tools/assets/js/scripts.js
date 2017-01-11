dfc278829f53aabdbfe9da4581e124b59745fe33
Cristian Iacobanu <cristianiacobanu@gmail.com>
2017-01-11 09:13:40 +0100

    Push latest gh-pages branch to master

--- server-dmz-web/var/www/network.tools/assets/js/scripts.js
+++ server-dmz-web/var/www/network.tools/assets/js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready( function () {
 
+	// Define Repository Name
+	var repository		= 'project_network';
+
+	// Define HTML element classes
+	var target			= '.md-file';
+	var error_message	= '.ajax-error';
+
+
 	// Get element class as path variable
-	var path = $('#md-file').attr('class').split(' ').join('/');
+	var path = $(target).attr('id').split(' ').join('/');
 
 	// Load readme content
 	$.ajax({
-		url: 'https://raw.githubusercontent.com/deadbok/project_network/'+path+'/README.md',
+		url: 'https://raw.githubusercontent.com/deadbok/'+repository+'/'+path+'/README.md',
 		dataType: 'text',
 		success: function(data) {
 
@@ -13,9 +21,39 @@ $(document).ready( function () {
 			var converter = new showdown.Converter({extensions: ['table', 'github']});
 
 			// Show html
-			$("#md-file").html(converter.makeHtml(data));
-			$('#md-file').anchorific();
+			$(target).html(converter.makeHtml(data));
+			// Show headings list (auto-generated)
+			$(target).anchorific();
+		},
+		error: function() {
+			// Add .ajax-not-working class to body
+			$('body').addClass('ajax-not-working');
+			// Add error message to .ajax-error element
+			$(error_message).fadeIn().html('<h4>Seems like your network settings are not able to connect to the internet.</h4>');
 		}
+	});
+
+	// Get <header> height
+	var header = $('header').outerHeight(true);
+
+	// Prevent default jump-to action for anchor links
+	// Animate scroll if link statrs with '#' character
+	$('.anchorific').on('click', 'a[href^=\\#]', function(event){
+	    event.preventDefault();
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top - header
+	    }, 500);
+		return false;
+	});
+
+	// Prevent default jump-to action for anchor links
+	// Animate scroll if link includes name
+	$('.md-file').on('click', 'a[href^=\\#]', function(event){
+	    event.preventDefault();
+	    $('html, body').animate({
+	        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - header
+	    }, 500);
+		return false;
 	});
 
 });
