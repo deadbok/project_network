bbd809497123839b3b22b5d16255c30e611bb385
Cristian Iacobanu <cristianiacobanu@gmail.com>
2017-01-22 17:10:01 +0100

    Update website files

--- server-dmz-web/var/www/network.tools/assets/js/scripts.js
+++ server-dmz-web/var/www/network.tools/assets/js/scripts.js
@@ -1,4 +1,33 @@
+"use strict";
+$(window).on('load', function(){
+
+	$('body').addClass('loading');
+
+});
+
 $(document).ready( function () {
+
+	////////////////////////////
+	// Start stuff
+	////////////////////////////
+
+	// Fixes some visual issues by displaying the content only when it is ready
+	$('body').removeClass('loading').addClass('ready');
+	$('header,main,footer').fadeIn(150);
+
+	////////////////////////////
+	// Mobile Navigation
+	////////////////////////////
+	$('.menu-icon').on( 'click', function() {
+		$(this).toggleClass('menu-active');
+	});
+
+	////////////////////////////
+	// GET Markdown File - AJAX
+	////////////////////////////
+
+	// Define GitHub User
+	var user			= 'deadbok';
 
 	// Define Repository Name
 	var repository		= 'project_network';
@@ -13,28 +42,58 @@ $(document).ready( function () {
 
 	// Load readme content
 	$.ajax({
-		url: 'https://raw.githubusercontent.com/deadbok/'+repository+'/'+path+'/README.md',
+		url: 'https://raw.githubusercontent.com/'+user+'/'+repository+'/'+path+'/README.md',
+		type: 'GET',
 		dataType: 'text',
 		success: function(data) {
+			// Display Headings List and Markdown File
+			$('.info, .md-file').fadeIn(200);
 
 			// Convert readme from markdown to html
-			var converter = new showdown.Converter({extensions: ['table', 'github']});
+			var converter = new showdown.Converter({extensions: ['table']});
 
 			// Show html
 			$(target).html(converter.makeHtml(data));
 			// Show headings list (auto-generated)
 			$(target).anchorific();
 		},
-		error: function() {
+		error: function(statusCode, errorThrown) {
 			// Add .ajax-not-working class to body
 			$('body').addClass('ajax-not-working');
+
 			// Add error message to .ajax-error element
-			$(error_message).fadeIn().html('<h4>Seems like your network settings are not able to connect to the internet.</h4>');
+			if (statusCode.status == 0) {
+				$(error_message).fadeIn(200).html('<h4>Seems like something went wrong!</h4><p>Check to see if you have <strong>Internet Access</strong></p>');
+				console.log('AJAX Error! Response: ' + statusCode.status);
+			} else if (statusCode.status == 404) {
+				$(error_message).fadeIn(200).html('<h4>Seems like something went wrong!</h4><p>Check to see if the <strong>README.md</strong> file exists or if the <strong>.md-file</strong> element has the correct IDs</p>');
+				console.log('AJAX Error! Response: ' + statusCode.status);
+			}
 		}
 	});
 
-	// Get <header> height
+	////////////////////////////
+	// Copy To Clipboard
+	////////////////////////////
+	$('.anchor').on( 'click', function() {
+		var clipboard = new Clipboard(this);
+
+	    clipboard.on('success', function(e) {
+	        alert(e);
+	    });
+
+	});
+
+	////////////////////////////
+	// Animat Scoll-To Action
+	////////////////////////////
+
+	// Get <header> and <footer> height
 	var header = $('header').outerHeight(true);
+	var footer = $('footer').outerHeight(true);
+
+	// Fix Body position issue caused by Fixed Header and Footer
+	$('body').css({paddingTop : header, paddingBottom : footer})
 
 	// Prevent default jump-to action for anchor links
 	// Animate scroll if link statrs with '#' character
